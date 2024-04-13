78fba64adfaf923097056c1540be6c5d28478b09
Elina Goncharova <ElinaAl.28@yandex.ru>
2024-04-13 16:50:26 +1200

    Написала код для решения задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
+﻿string [] Array = {"1234", "1567", "-2", "computer science"};
+string [] newArray = new string[Array.Length];
+int count = 0;
+{
+    foreach (string str in Array)
+    {
+        if (str.Length <= 3)
+        {
+            newArray[count] = str;
+            count++;
+        }
+    }
 
+}
+{
+    {
+        for (int i = 0; i < newArray.Length; i++)
+        Console.Write(newArray[i] + " ");
+    }
+}
